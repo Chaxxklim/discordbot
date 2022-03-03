6ea5b55357b7f0776e325a853bba3ce460ef629f
Chaxxklim <87353327+Chaxxklim@users.noreply.github.com>
2022-03-03 14:31:44 +0900

    write EPIPE error

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,17 +1,29 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { token, clientId, guildId } = require('./config.json');
+const fs = require('fs');
 
-const commands = [
-	new SlashCommandBuilder().setName('재린').setDescription('왜라고 대답합니다.'),
-	new SlashCommandBuilder().setName('뭐먹지').setDescription('아무거나 먹으라고 대답합니다.')
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-]
-	.map(command => command.toJSON());
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('성공.'))
-	.catch(console.error);
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
--- index.js
+++ index.js
@@ -10,21 +10,117 @@ const ytdl = require("discord-ytdl-core");
 const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource, AudioPlayerStatus, SubscriptionStatus } = require('@discordjs/voice');
 const player = createAudioPlayer();
 
+const Youtube = require('youtube-node');
+const youtube = new Youtube();
+//YouTube Data API v3 개인key값
+youtube.setKey(youtube_key);
+
 //봇 서버 시작될 때 한번만 실행
 client.once('ready', ()=> { 
     console.log('심재린 준비완료');
 })
 
+//else
+const fs = require('fs');
+const { join } = require('path');
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+const youtubeInfo_embed = {
+    color: '#00fa9a',
+    title: '재생목록에 추가됨!',
+    description: ''
+};
+
+
+//state
+client.on('ready', () => {
+    console.log('심재린 작동중...');
+    client.user.setActivity('작동', { type: 'PLAYING' })
+});
+
+//msg
+client.on('messageCreate', async msg => {
+    if (msg.content.indexOf('-p ') != -1) {
+        msg.react('👌')
+        let keyword = msg.content.substr(2).trim()
+        console.log(keyword)
+        let url = ''
+
+        //// 검색 옵션
+        youtube.addParam('type', 'video');
+
+        //search
+        youtube.search(keyword, 1, (err, result) => {
+            if (err) {
+                msg.reply('오류가 발생했습니다!')
+            }
+            let title = result["items"][0]["snippet"]["title"];
+            url = `https://www.youtube.com/watch?v=${result["items"][0]["id"]["videoId"]}`
+
+            let stream = ytdl(url, {
+                filter: "audioonly",
+                opusEncoded: true,
+                encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+            });
+
+            const connection = joinVoiceChannel({
+                channelId: msg.member.voice.channel.id,
+                guildId: msg.guild.id,
+                adapterCreator: msg.guild.voiceAdapterCreator
+            })
+
+            let resource = createAudioResource(stream);
+            player.play(resource)
+            player.on("error", console.error);
+            connection.subscribe(player)
+
+            msg.reply('재생합니다!')
+        });
+    } else if (msg.content == '-stop') {
+        player.stop()
+        msg.react('🛑')
+        msg.reply('재생을 중지합니다!')
+    } else if (msg.content == '-pause') {
+        player.pause()
+        msg.react('⏸')
+        msg.reply('일시정지합니다!')
+    } else if (msg.content == '-play') {
+        player.unpause()
+        msg.react('⏯')
+        msg.reply('다시 재생합니다!')
+    }
+});
+
 client.on('interactionCreate', async interaction => {
+	// if (!interaction.isCommand()) return;
+
+	// const { commandName } = interaction;
+
+	// if (commandName === '재린') {
+	// 	await interaction.reply('이재명 화이팅!');
+	// } else if (commandName === '뭐먹지'){
+    //     await interaction.reply('아무거나 먹어라');
+    // }
+
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+    const command = client.commands.get(interaction.commandName);
 
-	if (commandName === '재린') {
-		await interaction.reply('이재명 화이팅!');
-	} else if (commandName === '뭐먹지'){
-        await interaction.reply('아무거나 먹어라');
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
+
 });
 
 
