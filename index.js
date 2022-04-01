4e1db272369834ca0a05733af59272acdad8491c
Chaxxklim <87353327+Chaxxklim@users.noreply.github.com>
2022-04-01 17:51:30 +0900

    코드 정리

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 // const { Client, Intents } = require('discord.js');
 const { token, youtube_key } = require('./config.json');
 // const client = new Client({ intents : [Intents.FLAGS.GUILDS] });
-
 const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS]
